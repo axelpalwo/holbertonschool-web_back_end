488ad872df51638a0bf8097e1034fd6c26f810ba
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-11-13 14:08:54 +0000

    fourth task

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,5 +1,8 @@
 export default function returnHowManyArguments(...args) {
   let i = 0;
-  for (const arg of args) i += 1;
+  for (const arg of args) {
+    i += 1;
+    console.log(arg)
+  }
   return i;
 }
