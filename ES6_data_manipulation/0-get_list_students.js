32165288b8271d4bf790f0d4ca0b61469d868751
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-11-20 13:50:07 +0000

    new project

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,20 @@
 export default function getListStudents() {
-    const objarr = [
-        {
-            firstName: 'Guillaume',
-            id: 1,
-            location: 'San Francisco',
-        },
-        {
-            firstName: 'James',
-            id: 2,
-            location: 'Columbia',
-        },
-        {
-            firstName: 'Serena',
-            id: 5,
-            location: 'San Francisco',
-        },
-    ];
-    return objarr;
+  const objarr = [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
+  return objarr;
 }
