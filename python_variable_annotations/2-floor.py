16df6f3b4ab4199e1aa0b8baf0d573112891c02a
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-02 18:00:35 +0000

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to take a float and return its floor.
+"""
+
+
+def floor(a: float) -> int:
+    """Returns a float's floor."""
+    return int(a)
