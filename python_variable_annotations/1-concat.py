676a7c8e9619d4efdae992dc9ea6714320c5046f
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-02 17:58:16 +0000

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to concatenate two strings.
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """Returns a string concatenated."""
+    return a + b
