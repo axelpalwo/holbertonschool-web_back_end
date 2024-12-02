f550738381cd8b4c454864dbf9e603263f709220
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-02 18:01:54 +0000

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to take a float and return its string representation.
+"""
+
+
+def to_str(a: float) -> str:
+    """Returns a float's string representation."""
+    return str(a)
