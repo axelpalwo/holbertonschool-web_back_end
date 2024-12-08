7de7a40f3b863a76dbd039f55148d3ebd3dd87d2
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-08 05:42:31 +0000

    7th task

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """Import List and Union for variable annotation"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple with a string and the square of an int/float as a float.
     """
