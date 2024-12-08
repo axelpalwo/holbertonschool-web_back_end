eb85723974fe66e0f58104af80a6656fe5d967be
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-08 06:00:19 +0000

    8th task

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Import Callable for variable annotation"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by a given multiplier.
+    """
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+
+    return multiplier_function
