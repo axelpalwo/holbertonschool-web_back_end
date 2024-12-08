57c6c6dd2d21aeeea7183745b62397201b5f1d4b
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-08 06:05:59 +0000

    9th task

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Import Callable, Sequence, List and Tuple for variable annotation"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples, where each tuple contains an element of the iterable
+    and the length of that element.
+    """
+    return [(i, len(i)) for i in lst]
