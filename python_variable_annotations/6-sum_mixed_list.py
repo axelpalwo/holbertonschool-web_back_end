78b3925439f0af6c9c7798ddf0821ad3b018ec0f
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-08 05:35:31 +0000

    6th task

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Import List for variable annotation"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of a list of floats and integers."""
+    return sum(mxd_lst)
