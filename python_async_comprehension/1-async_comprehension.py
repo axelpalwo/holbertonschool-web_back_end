9a9c74fdf1781db6b8bd1e24d236977a89126d31
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-14 22:59:13 +0000

    task one

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 import asyncio
-from typing import List
-from 0-async_generator import async_generator  # Import the generator
+import random
+from typing import Generator, List
 
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Coroutine that asynchronously generates 10 random numbers.
+    It waits for 1 second before yielding each random number.
+    """
+
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
 
 async def async_comprehension() -> List[float]:
     """
