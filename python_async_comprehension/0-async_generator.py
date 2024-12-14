be1a1d212065549ccd3e20acb526077beeddfd47
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-12-14 22:30:50 +0000

    doc

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,6 +11,7 @@ async def async_generator() -> Generator[float, None, None]:
     It waits for 1 second before yielding each random number.
     """
 
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
