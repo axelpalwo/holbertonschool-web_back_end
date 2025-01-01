927c9e413572de9a6ebc490e149f152be0809bd9
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-01-01 13:41:40 +0000

    fourth task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    
+    Creates n tasks
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
 
