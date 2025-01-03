8ccde757975511dd39b0d2c6136994eef55488a0
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-01-03 18:53:11 +0000

    doc mod

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""First task of this project"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Defines the start and end indexes"""
+
+    
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
