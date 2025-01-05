79819a8afe9453f01de937fe5c7842fe72315ba7
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-01-05 21:07:17 +0000

    task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Looks in a collection many documents"""
-    return list(mongo_collection.find({ "topic": topic}))
+    return list(mongo_collection.find({ "topics": topic}))
