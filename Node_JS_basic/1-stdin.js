5543f6d07d5d92f27860793308c8dc3aaaced20f
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-01-26 16:02:07 +0000

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
 
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\r\n`);
+  process.stdout.write(`Your name is: ${input}\r`);
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\r');
 });
 
 process.stdin.on('end', () => {
