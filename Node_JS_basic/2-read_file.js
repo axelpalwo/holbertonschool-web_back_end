8d58f8b5a8be5508599f9814236b9034f1bccbe9
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-01-26 16:50:10 +0000

    task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8').trim();
+    const lines = data.split('\n');
+
+    if (lines.length <= 1) throw new Error('Cannot load the database');
+
+    let NUMBER_OF_STUDENTS = 0;
+    let CS = 0;
+    let SWE = 0;
+    const ListCS = [];
+    const ListSWE = [];
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      NUMBER_OF_STUDENTS++;
+      if (field === 'CS') {
+        CS++;
+        ListCS.push(firstname);
+      } else if (field === 'SWE') {
+        SWE++;
+        ListSWE.push(firstname);
+      }
+    });
+
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+    console.log(`Number of students in CS: ${CS}. List: ${ListCS.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWE}. List: ${ListSWE.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
