395c42c2e8eaaff2d9166166a8ddfa3ef286da81
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-01-26 17:33:04 +0000

    task 5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,37 @@
+// 5-http.js
+
+const http = require('http');
+const { countStudents } = require('./3-read_file_async'); // Importa la función countStudents
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Maneja la solicitud GET
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.end('Hello Holberton School!\n');
+    } else if (req.url === '/students') {
+      // Llama a countStudents y maneja la promesa
+      countStudents('./database.csv')
+        .then((csvdata) => {
+          res.end(
+            `This is the list of our students\n${
+              csvdata}`,
+          );
+        })
+        .catch((error) => {
+          res.statusCode = 500;
+          res.end(error.message);
+        });
+    }
+  } else {
+    res.statusCode = 405;
+    res.end('Method Not Allowed');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on http://localhost:1245');
+});
+
+module.exports = app;
